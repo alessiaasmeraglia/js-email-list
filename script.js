0a2330d5f4bbb25f04ca076a35bb7bf24c09ed23
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-15 15:08:38 +0200

    Recupero elementi dal DOM

---
+++ script.js
@@ -0,0 +1,7 @@
+/* 
+- creo un array vuoto
+- faccio 10 fetch
+- ogni risposta la metto nell’array
+- quando arrivo a 10 email, le stampo nella ul */
+const emailList = document.getElementById("emailList");
+const generateBtn = document.getElementById("generateBtn");
